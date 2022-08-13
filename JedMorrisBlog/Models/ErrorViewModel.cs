f49063e95822d5ab7f79a1ffdc1cfbb760c7ff94
jedmorris <jemorris110@gmail.com>
2022-08-12 20:41:41 -0700

    first commit

---
+++ JedMorrisBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JedMorrisBlog.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
